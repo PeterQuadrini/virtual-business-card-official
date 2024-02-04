2833fb78ee367d76029802f712364e35c6ece14d
PeterQuadrini <quadrinipietro@gmail.com>
2024-02-04 15:09:30 +0100

    kjkjkjjbn

--- src/static/data/Data.js
+++ src/static/data/Data.js
@@ -8,6 +8,16 @@ const users = {
     name: 'Pietro Quadrini',
     image: '/images/pietroQuadrini.png',
     file: '/vcf/pietroQuadrini.vcf'
+  },
+  'ajd48s7dtw': {
+    name: 'Giovanni Quadrini',
+    image: '/images/giovanniQuadrini.png',
+    file: '/vcf/giovanniQuadrini.vcf'
+  },
+  'ajd48s7d9f': {
+    name: 'Marco Quadrini - Tecnosan',
+    image: '/images/marcoQuadrini.png',
+    file: '/vcf/marcoQuadrini.vcf'
   }
 }
 
